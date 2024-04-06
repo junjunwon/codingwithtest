37266a85fbfcd54b905c3f9aab0ff60168c12365
junjunwon <45681336+junjunwon@users.noreply.github.com>
2024-04-06 17:44:20 +0900

    initialize springboot project

---
+++ src/main/java/com/codingwithtest/codewithtest/CodewithtestApplication.java
@@ -0,0 +1,13 @@
+package com.codingwithtest.codewithtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodewithtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodewithtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingwithtest/codewithtest/CodewithtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingwithtest.codewithtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodewithtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
